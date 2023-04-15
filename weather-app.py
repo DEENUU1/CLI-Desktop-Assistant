d629f67d79e62841fb30838d0a456c014513896d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-15 16:37:44 +0200

    Feature | Returning weather data | I used api from openweathermap that allows me to return temperature, description, pressure and wind speed

--- weather-app.py
+++ weather-app.py
@@ -4,6 +4,9 @@ from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
+
 @dataclass
 class WeatherData:
     temp: float
@@ -12,4 +15,19 @@ class WeatherData:
     wind_speed: float
 
 
+class GetWeatherData:
+    def __init__(self, city: str):
+        self.city = city
+        self.api_key = os.getenv("WEATHER_API_KEY")
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}"
 
+    def get_data(self) -> WeatherData:
+        response = get(self.url)
+        data = json.loads(response.text)
+
+        temp = data["main"]["temp"]
+        desc = data["weather"][0]["description"]
+        pressure = data["main"]["pressure"]
+        wind_speed = data["wind"]["speed"]
+
+        return WeatherData(temp, desc, pressure, wind_speed)

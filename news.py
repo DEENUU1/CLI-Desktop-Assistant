ca6c3c4280e6bdf2eb04132c4ce5f5727e2c16e2
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-15 17:14:11 +0200

    Feature | Returning popular news | GetNewsData class allows me to return the most popular news from a specified country from newsapi

---
+++ news.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+from requests import get
+import json
+from dataclasses import dataclass
+
+
+load_dotenv()
+
+
+@dataclass
+class NewsData:
+    title: str
+    author: str
+    url: str
+
+
+class GetNewsData:
+    def __init__(self, country_code):
+        self.country_code = country_code
+        self.api_key = os.getenv("NEWS_API_KEY")
+        self.url = f"https://newsapi.org/v2/top-headlines?country={country_code}&apiKey={self.api_key}"
+
+    def get_news(self):
+        result = get(self.url)
+        result_json = json.loads(result.content)
+        articles = result_json["articles"]
+        all_articles = []
+        for article in articles:
+            title = article["title"]
+            author = article["author"]
+            url = article["url"]
+
+            all_articles.append(
+                NewsData(title=title,
+                         author=author,
+                         url=url)
+            )
+
+        return all_articles
